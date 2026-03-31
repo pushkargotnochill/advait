2cd9dbccd0cefc5934af84791189ac68d7b71f7d
PUSHKAR BATRA <pushkar121207@gmail.com>
2026-03-31 22:40:23 +0530

    Add files via upload

---
+++ apllied_ai_assignment.py
@@ -0,0 +1,30 @@
+from transformers import pipeline
+
+
+sentiment_report = pipeline("sentiment-analysis")
+result = sentiment_report("i love everyone")
+print(result)
+
+
+summarizer = pipeline( model="facebook/bart-large-cnn")
+text = """Because Ichigo lacks a traditional, proactive goal, critics sometimes label him as a passive character who only moves when the plot demands it. Yet, this is precisely what makes him so compelling. Ichigo is a normal teenager thrust into extraordinary, terrifying circumstances. He values his normal life, his schoolwork, and his friends above the politics of the supernatural Soul Society.
+
+When he loses his powers after defeating the central villain Sosuke Aizen, he falls into a realistic, profound depression. This highlights that his powers were never just weapons to him; they were his insurance against powerlessness. Watching him claw his way back from despair repeatedly proves that his greatest strength is not his massive spiritual energy, but his sheer, unyielding resolve.
+"""
+summary = summarizer(text, max_length =30, min_length = 20, do_sample = False)
+
+print(summary[0]['summary_text'])
+
+
+
+qa = pipeline("question-answering", model="deepset/roberta-base-squad2")
+
+context_1 = """
+Because Ichigo lacks a traditional, proactive goal, critics have occasionally 
+accused him of being a bland protagonist. When he loses his powers after 
+defeating the central villain Sosuke Aizen, Ichigo trains to recover them 
+using a method called the Fullbring.
+"""
+
+result = qa(question="How does Ichigo recover his powers?", context=context_1)
+print(result['answer'])
